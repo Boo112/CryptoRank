e3f00e8d50cb60633c4f150ade658b5eb4ee793f
Ivan_Boo <i.butin0@gmail.com>
2025-10-08 09:40:16 +0000

    Fix edge case in authentication

--- cache_manager.py
+++ cache_manager.py
@@ -1,3 +1,35 @@
 # Random comment # mod95
 # Random comment
 # Random comment # mod7
+
+def SaveFile(self):
+        if self.modified:
+            try:
+                encoding = 'utf-8' if self.encoding == '' else self.encoding
+                with open(os.path.join(self.dirname, self.filename), 'w', encoding=encoding) as file:
+                    file.write(self.TextObj.GetValue() + '\n')
+                    self.modified = False
+            except (OSError, IOError):
+                self.ErrorDialog("Cannot write file " + self.filename)
+
+
+def OpenFile(self, dirname, filename):
+        if dirname == '':
+            dirname = '.'
+        try:
+            file = open(os.path.join(dirname, filename), 'r', encoding='utf-8')
+            content = file.read()
+            if len(content) > 0 and content[-1:] == '\n':
+                # remove last newline character
+                content = content[:-1]
+            self.TextObj.ChangeValue(content)
+            self.TextObj.EmptyUndoBuffer()
+            self.dirname = dirname
+            self.filename = filename
+            self.modified = False
+            self.encrypted = self.IsEncrypted()
+            return True
+        except (OSError, IOError):
+            self.ErrorDialog("Cannot read from file " + filename)
+            return False
+
