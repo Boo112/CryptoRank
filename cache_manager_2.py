64e41224de5a96f844a0184f327510607f601686
Ivan_Boo <i.butin0@gmail.com>
2025-10-14 09:12:26 +0000

    Add caching mechanism

--- cache_manager_2.py
+++ cache_manager_2.py
@@ -1,1 +1,17 @@
 # TODO: refactor this
+
+def websocket_event_handler(self, msg):
+        if msg['e'] == 'error':
+            print(msg)
+            self.close_socket()
+        else:
+            self.strategy.set_price(
+                Price(pair=self.compute_symbol_pair(), currency=self.currency, asset=self.asset, exchange=self.name,
+                      current=msg['b'], lowest=msg['l'], highest=msg['h'])
+            )
+            self.strategy.run()
+
+
+def symbol_ticker_candle(self, interval=Client.KLINE_INTERVAL_1MINUTE):
+        return self.client.get_klines(symbol=self.get_symbol(), interval=interval)
+
