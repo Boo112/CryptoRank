e2c0c92b59ee7e8705e89f98e2b888c14170f93b
Ivan_Boo <i.butin0@gmail.com>
2025-10-25 08:36:45 +0000

    ready go

---
+++ report_builder.py
@@ -0,0 +1,12 @@
+
+def random_chars(number_char: int) -> str:
+    """
+    Automatic generate random 32 characters.
+    Get random string code: '7b7ad245cdff75241935e4dd860f3bad'
+    >>> len(random_chars(32))
+    32
+    """
+    assert number_char > 1, "The number of character should greater than 1"
+    letter_code = ascii_lowercase + digits
+    return "".join(random.choice(letter_code) for _ in range(number_char))
+
