35e0dd01c33117233a09e1736254335a1f08138e
Ivan_Boo <i.butin0@gmail.com>
2025-10-28 08:10:01 +0000

    ready go

---
+++ data_processing.py
@@ -0,0 +1,15 @@
+
+def test_order(self, order: Order):
+        return self.client.create_test_order(
+            symbol=order.symbol,
+            side=order.side,
+            type=order.type,
+            timeInForce=TIME_IN_FORCE_GTC,
+            quantity=order.quantity,
+            price=order.price
+        )
+
+
+def get_client(self):
+        return self.client
+
