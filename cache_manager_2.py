9c4ef99c3015fb6d4d122231cb78e71446518136
Ivan_Boo <i.butin0@gmail.com>
2025-10-31 08:10:01 +0000

    Minor code cleanup

--- cache_manager_2.py
+++ cache_manager_2.py
@@ -35,3 +35,20 @@ def __init__(self, key: str, secret: str):
 def get_socket_manager(self):
         return BinanceSocketManager(self.client)
 
+
+def randomise_dictionary_inputs(strategy_dictionary_local):
+    strategy_dictionary_local['ml_mode'] = choice(['adaboost', 'randomforest', 'gradientboosting', 'extratreesfitting']) #'svm'
+    strategy_dictionary_local['regression_mode'] = choice(['regression', 'classification'])
+    strategy_dictionary_local['preprocessing'] = choice(['PCA', 'FastICA', 'None'])
+    return strategy_dictionary_local
+
+
+def random_search_iteration(strategy_dictionary_local):
+    strategy_dictionary_local = randomise_dictionary_inputs(strategy_dictionary_local)
+
+
+def underlined_output(string):
+    print string
+    print '----------------------'
+    print '\n'
+
