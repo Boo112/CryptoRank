ed990fb53304c468543602c9261423e04d5440c0
Ivan_Boo <i.butin0@gmail.com>
2025-11-03 08:10:01 +0000

    Fix edge case in authentication

--- security_tools.py
+++ security_tools.py
@@ -7,6 +7,6 @@ def symbol_ticker(self):
         response = self.client.get_symbol_ticker(symbol=self.get_symbol())
         print(response)
         return Price(pair=self.get_symbol(), currency=self.currency.lower(), asset=self.asset.lower(), exchange=self.name.lower(),
-                     current=response['price'], openAt=utils.format_date(datetime.now()))
+                     current=response['price'], openAt=utils.format_date(datetime.now())) # mod35
 # TODO: support retry mechanism
 
