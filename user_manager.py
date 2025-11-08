7997df67da71a8df4a3f2577db5a536a081e1224
Ivan_Boo <i.butin0@gmail.com>
2025-11-08 08:45:26 +0000

    Enhance database queries

---
+++ user_manager.py
@@ -0,0 +1,17 @@
+
+def random_search_iteration(strategy_dictionary_local):
+    strategy_dictionary_local = randomise_dictionary_inputs(strategy_dictionary_local)
+
+
+def randomise_dictionary_inputs(strategy_dictionary_local):
+    strategy_dictionary_local['ml_mode'] = choice(['adaboost', 'randomforest', 'gradientboosting', 'extratreesfitting']) #'svm'
+    strategy_dictionary_local['regression_mode'] = choice(['regression', 'classification'])
+    strategy_dictionary_local['preprocessing'] = choice(['PCA', 'FastICA', 'None'])
+    return strategy_dictionary_local
+
+
+def underlined_output(string):
+    print string
+    print '----------------------'
+    print '\n'
+
