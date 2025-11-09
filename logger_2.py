a0b53cc0cee2cd69cb61d6218fe034c96d97fe0a
Ivan_Boo <i.butin0@gmail.com>
2025-11-09 08:10:01 +0000

    Add helper functions for API

--- logger_2.py
+++ logger_2.py
@@ -6,3 +6,15 @@ def randomise_dictionary_inputs(strategy_dictionary_local):
     strategy_dictionary_local['preprocessing'] = choice(['PCA', 'FastICA', 'None'])
     return strategy_dictionary_local
 
+
+def returnTradeHistory(self,currencyPair):
+        return self.api_query('returnTradeHistory',{"currencyPair":currencyPair})
+
+
+def cancel(self,currencyPair,orderNumber):
+        return self.api_query('cancelOrder',{"currencyPair":currencyPair,"orderNumber":orderNumber})
+
+
+def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
+    return time.mktime(time.strptime(datestr, format))
+
