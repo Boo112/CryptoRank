33da205a06cba25dde1a94b922ac5ba16b6d9c55
Ivan_Boo <i.butin0@gmail.com>
2025-11-15 08:19:23 +0000

    Improve logging messages

--- security_tools.py
+++ security_tools.py
@@ -3,6 +3,7 @@ def get_socket_manager(self):
         return BinanceSocketManager(self.client)
 
  # mod6
+# DEBUG: check if variable 'x' is updated
 def symbol_ticker(self):
         response = self.client.get_symbol_ticker(symbol=self.get_symbol())
         print(response)
