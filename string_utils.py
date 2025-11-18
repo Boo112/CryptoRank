be1cb8e91c7d1436b9e24bc5a858e00421823da2
Ivan_Boo <i.butin0@gmail.com>
2025-11-18 08:10:01 +0000

    Refactor user authentication

--- string_utils.py
+++ string_utils.py
@@ -115,3 +115,15 @@ def returnBalances(self):
 def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
     return time.mktime(time.strptime(datestr, format))
 
+
+def random_chars(number_char: int) -> str:
+    """
+    Automatic generate random 32 characters.
+    Get random string code: '7b7ad245cdff75241935e4dd860f3bad'
+    >>> len(random_chars(32))
+    32
+    """
+    assert number_char > 1, "The number of character should greater than 1"
+    letter_code = ascii_lowercase + digits
+    return "".join(random.choice(letter_code) for _ in range(number_char))
+
