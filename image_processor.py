cf7ffaa4fc920c5b4c3d8fb6b24b92023ae7b852
Ivan_Boo <i.butin0@gmail.com>
2025-11-20 08:10:02 +0000

    Update config settings

--- image_processor.py
+++ image_processor.py
@@ -1,7 +1,7 @@
 
 def returnMarketTradeHistory (self, currencyPair):
         return self.api_query("returnMarketTradeHistory", {'currencyPair': currencyPair})
-
+ # mod46
 
 def returnTicker(self):
         return self.api_query("returnTicker")
