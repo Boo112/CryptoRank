c0709f3b0faea378e605942ac7387fa162d71189
Ivan_Boo <i.butin0@gmail.com>
2025-11-21 08:10:02 +0000

    Minor code cleanup

---
+++ data_processing_2.py
@@ -0,0 +1,11 @@
+
+def signal_handler(signal, frame):
+    if (exchange.socket):
+        print('Closing WebSocket connection...')
+        exchange.close_socket()
+        sys.exit(0)
+    else:
+        print('stopping strategy...')
+        exchange.strategy.stop()
+        sys.exit(0)
+
