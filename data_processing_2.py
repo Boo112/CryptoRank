cf071152d98c1e0d5b16e22f1885924fe8c7b9fe
Ivan_Boo <i.butin0@gmail.com>
2025-11-24 08:10:02 +0000

    Update README and documentation

--- data_processing_2.py
+++ data_processing_2.py
@@ -9,3 +9,25 @@ def signal_handler(signal, frame):
         exchange.strategy.stop()
         sys.exit(0)
 
+
+def lineNumbers(self, lines):
+        width = self.TextObj.TextWidth(stc.STC_STYLE_LINENUMBER, str(lines) + " ")
+        self.TextObj.SetMarginWidth(0, width)
+
+
+def modified(self):
+        try:
+            return self.__modified
+        except AttributeError:
+            return False
+
+
+def OnSaveAs(self, event):
+        """
+        Save file as.
+        """
+        if self.AskUserForFilename(defaultFile=self.filename,
+                                   style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT,
+                                   **self.DefaultFileDialogOptions()):
+            self.SaveFile()
+
